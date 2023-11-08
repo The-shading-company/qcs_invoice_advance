80e0a915d7b9cae5c105c84cf0acc431e8039589
Vivek Digambernath <30491190+quarkcs@users.noreply.github.com>
2023-11-08 14:55:07 +0400

    Update hooks.py
    added CustomPurchaseOrder class to fix subcontracting PO creation from Material Request

--- qcs_invoice_advance/hooks.py
+++ qcs_invoice_advance/hooks.py
@@ -95,9 +95,9 @@ doctype_js = {"Sales Order": "public/js/sales_order.js",
 # ---------------
 # Override standard doctype classes
 
-# override_doctype_class = {
-#	"ToDo": "custom_app.overrides.CustomToDo"
-# }
+override_doctype_class = {
+	"Purchase Order": "qcs_invoice_advance.overrides.CustomPurchaseOrder"
+}
 
 # Document Events
 # ---------------
