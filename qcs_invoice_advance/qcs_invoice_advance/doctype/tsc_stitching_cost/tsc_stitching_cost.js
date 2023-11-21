f990144e38bfd8e3c372c0896cd44dd3e2e0152f
hiraniya <hiranis@gmail.com>
2023-11-21 17:34:43 +0400

    Resolve conflicts

---
+++ qcs_invoice_advance/qcs_invoice_advance/doctype/tsc_stitching_cost/tsc_stitching_cost.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Quark Cyber Systems FZC and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('TSC Stitching Cost', {
+	// refresh: function(frm) {
+
+	// }
+});
