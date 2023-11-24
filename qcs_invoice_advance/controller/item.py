e338c4a1f73debdcf2dea0e8b288a3222a1ea38d
Vivek Digambernath <30491190+quarkcs@users.noreply.github.com>
2023-11-24 09:27:20 +0400

    Update item.py

--- qcs_invoice_advance/controller/item.py
+++ qcs_invoice_advance/controller/item.py
@@ -34,7 +34,8 @@ def create_bom(self, event):
                 for j in range(0, len(att_tab)):
                     if (att_tab[j].get("attribute_value") == value):
                         size.append(tab[i].get("abbr"))
-
+        frappe.errprint("size:-")
+        frappe.errprint(size)
         stich = frappe.get_all("TSC Stitching Cost")
         for i in stich:
             s_doc = frappe.get_doc("TSC Stitching Cost", i)
