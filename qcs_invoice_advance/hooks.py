0cfbd9da4d84734afc885d149b96729e338e3ac7
Vivek Digambernath <30491190+quarkcs@users.noreply.github.com>
2023-11-24 10:39:00 +0400

    Update hooks.py

--- qcs_invoice_advance/hooks.py
+++ qcs_invoice_advance/hooks.py
@@ -108,7 +108,12 @@ doc_events = {
 		"after_insert": "qcs_invoice_advance.controller.item.create_bom",
         	"on_update": "qcs_invoice_advance.controller.item.update_bom",
         	"on_trash": "qcs_invoice_advance.controller.item.delete_bom",
-	}
+	},
+	"BOM": {
+         	"on_submit": "qcs_invoice_advance.controller.item.add_sale_price",
+         	"on_update": "qcs_invoice_advance.controller.item.add_sale_price",
+
+   	}
 }
 
 
