e750d85bf788dfee354643b5320b760e9bae951b
Vivek <vivek@digambernath.org>
2024-02-23 15:20:24 +0400

    Update hooks.py

--- qcs_invoice_advance/hooks.py
+++ qcs_invoice_advance/hooks.py
@@ -119,7 +119,7 @@ doc_events = {
 		"validate": "qcs_invoice_advance.controller.item.tsc_custom_accounts"
 	},
 	"Quotation": {
-		#"validate": "qcs_invoice_advance.controller.item.add_margins",
+		"validate": "qcs_invoice_advance.controller.item.add_margins",
 		"after_insert": "qcs_invoice_advance.controller.item.add_quote_link"
 	}
 }
