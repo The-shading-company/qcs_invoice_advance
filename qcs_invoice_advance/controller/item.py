8e3eb35123baae9418e8d366a9d9ae68490b840d
Vivek <vivek@digambernath.org>
2024-03-11 14:09:38 +0400

    added tax

--- qcs_invoice_advance/controller/item.py
+++ qcs_invoice_advance/controller/item.py
@@ -542,7 +542,7 @@ def create_payment_link(dt, dn, amt, purpose):
 
 
 @frappe.whitelist()
-def create_sub_po(dt, dn, parent_item, can_item, qty, uom, line_id):
+def create_sub_po(dt, dn, parent_item, can_item, qty, uom, line_id, supplier):
 	c_type = ""
 	c_size = ""
 	c_cost = 0
@@ -559,7 +559,7 @@ def create_sub_po(dt, dn, parent_item, can_item, qty, uom, line_id):
 	po.company = so.company
 	po.is_subcontracted = 1
 	po.transaction_date = so.transaction_date
-	po.supplier = "Edge And Curve Furniture Upholstery"
+	po.supplier = supplier
 	po.schedule_date = so.delivery_date
 	po.append("items",{
 		"fg_item": can_item,
@@ -573,11 +573,10 @@ def create_sub_po(dt, dn, parent_item, can_item, qty, uom, line_id):
 		"sales_order": so.name,
 		"sales_order_item": line_id
 	})
+	po.taxes_and_charges = "UAE VAT 5%"
 	po.run_method("set_missing_values")
 	po.run_method("calculate_taxes_and_totals")
 	po.save()
-					
-
 	return po
 
 
