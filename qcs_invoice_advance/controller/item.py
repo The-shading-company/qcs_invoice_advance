2b73a88693bf204ff522b1fb05ffece6371727e6
Vivek <vivek@digambernath.org>
2024-03-16 13:53:56 +0400

    Update item.py

--- qcs_invoice_advance/controller/item.py
+++ qcs_invoice_advance/controller/item.py
@@ -641,11 +641,11 @@ def update_item_price_based_on_bom():
     }, fields=["name", "item_code"])
 
     for item_price in item_prices:
-	bom_name = frappe.get_value("BOM", {"item": item_price.item_code, "is_default": 1, "docstatus": 1}, "name", order_by="creation desc")
-        if bom_name:
-		bom = frappe.get_doc("BOM", bom_name)
-		bom.update_cost()
-		bom.update(ignore_permissions=True)
+	    bom_name = frappe.get_value("BOM", {"item": item_price.item_code, "is_default": 1, "docstatus": 1}, "name", order_by="creation desc")
+	    if bom_name:
+		    bom = frappe.get_doc("BOM", bom_name)
+		    bom.update_cost()
+		    bom.update(ignore_permissions=True)
 
 
     frappe.db.commit()  # Commit changes to the database
