1926045943c7c163c0d96bb937ee4cdaa91c8818
Vivek <vivek@digambernath.org>
2024-09-01 11:12:52 +0400

    Update quotation.py

--- qcs_invoice_advance/controller/quotation.py
+++ qcs_invoice_advance/controller/quotation.py
@@ -1,6 +1,6 @@
 import frappe
 
-def update_service_call(self, event)
+def update_service_call(self, event):
     doc = frappe.get_doc("TSC Service Call", self.custom_tsc_service_call)
     doc.quote = self.name
     doc.save(ignore_permissions=True)
