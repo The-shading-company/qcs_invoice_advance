aa3a06004c576e7f424de56053472620ad9b40c3
Vivek <vivek@digambernath.org>
2025-02-22 14:57:51 +0400

    removed status_time_log table

--- qcs_invoice_advance/qcs_invoice_advance/doctype/tsc_service_call/tsc_service_call.py
+++ qcs_invoice_advance/qcs_invoice_advance/doctype/tsc_service_call/tsc_service_call.py
@@ -28,69 +28,6 @@ class TSCServiceCall(Document):
 			#		"status_time_log" : item
 			#	})
 
-		if len(self.status_time_log) > 0:
-			tab = self.status_time_log
-			check = []
-			item = []
-			for i in range(0, len(tab)):
-				item.append({
-					"status": tab[i].get("status"),
-					"date": tab[i].get("date"),
-					"time": tab[i].get("time"),
-					"hours": tab[i].get("hours")
-				})
-				if (self.status == tab[i].get("status")):
-					check.append(1)
-				else:
-					check.append(0)
-			if (1 not in check):
-
-				system_current_time = timezone_converted_datetime(datetime.now())
-				current_time = system_current_time.time()
-				# previous_time = datetime.strptime(tab[i-1].get("time"), '%H:%M:%S')
-				# current_time1 = datetime.strptime(current_time, '%H:%M:%S')
-
-				#current_time_str = datetime.now().strftime('%H:%M:%S')
-				current_date_str = date.today()
-				
-				previous_time_str = tab[i-1].get("time")
-				previous_date_str = tab[i-1].get("date")
-				if isinstance(previous_time_str, timedelta):
-					pass
-				else:
-					previous_time_str = str_to_timedelta(previous_time_str)
-
-				if isinstance(previous_date_str, date):
-					pass
-				else:
-					previous_date_str = datetime.strptime(previous_date_str, "%Y-%m-%d")
-
-				# previous_date_str = str(previous_date_str1)
-
-				frappe.errprint(type(previous_time_str))
-				frappe.errprint(type(previous_date_str))
-				# previous_datetime = datetime.strptime(str(previous_date_str) + ' ' + previous_time_str, '%Y-%m-%d %H:%M:%S')
-				# frappe.errprint(previous_datetime)
-
-				combined_datetime = convert_and_combine(previous_date_str, previous_time_str)
-				current_datetime = datetime.combine(current_date_str, current_time)
-
-				#current_datetime = datetime.strptime(current_date_str + ' ' + current_time_str, '%Y-%m-%d %H:%M:%S')
-				frappe.errprint(combined_datetime)
-				time_diff = current_datetime - combined_datetime
-				hours = time_diff.total_seconds() / 3600
-
-				item.append({
-					"status": self.status,
-					"date": today(),
-					"time": current_time,
-					"hours": hours
-				})
-			frappe.errprint(item)
-			if (item):
-				self.update({
-					"status_time_log" : item
-				})
 
 
 # Assuming previous_date_str is a date object and previous_time_str is a timedelta
