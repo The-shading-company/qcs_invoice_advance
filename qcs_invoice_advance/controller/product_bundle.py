fe695965871bfc51f44cbd098efd5826e7553cbb
Vivek <vivek@digambernath.org>
2025-02-22 10:42:52 +0400

    Update product_bundle.py

--- qcs_invoice_advance/controller/product_bundle.py
+++ qcs_invoice_advance/controller/product_bundle.py
@@ -6,7 +6,7 @@ def cal_cost(self, event):
         tab = self.items
         avg_rate = []
         for i in range(0, len(tab)):
-            item_cost = (tab[i].get("qty") or 0) * (tab[i].get("custom_item_cost") or 0)
+            item_cost = float((tab[i].get("qty") or 0) * (tab[i].get("custom_item_cost") or 0))
             tab[i].custom_item_total_cost = float(item_cost)
             avg_rate.append(float(item_cost))
         self.custom_item_total_cost = sum(avg_rate)
