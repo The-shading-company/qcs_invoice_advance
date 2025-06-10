795ebe2cbfd4a614637592386baf0977ebbc3677
stephen foot <stephen.foot@googlemail.com>
2025-06-10 07:25:05 +0000

    Updated logo cost check as was taking 8 seconds previously.

--- qcs_invoice_advance/qcs_invoice_advance/doctype/logo_costing_form/logo_costing_form.py
+++ qcs_invoice_advance/qcs_invoice_advance/doctype/logo_costing_form/logo_costing_form.py
@@ -3,28 +3,44 @@
 import frappe
 from frappe.model.document import Document
 from frappe.utils import now_datetime
-
+from collections import defaultdict
 
 class LogoCostingForm(Document):
 	pass
 
-
+#improved speed for checking logo cost
 def update_cost():
-	cost_doc = frappe.get_all("Logo Costing Form", fields=["name"])
+	
+	# Preload multiplier once
 	setup_doc = frappe.get_doc("TSC Logo Setup")
 	multiplier = setup_doc.selling_multiplier or 1
-	if cost_doc:
-		for j in cost_doc:
-			cost_doc1 = frappe.get_doc("Logo Costing Form", j.get("name"))
-			if cost_doc1.logo_costing_id and cost_doc1.row_name:
-				doc = frappe.get_doc("TSC Logo Costing", cost_doc1.logo_costing_id)
-				tab = doc.logos
-				if (tab):
-					for i in range(0, len(tab)):
-						if (tab[i].get("name") == cost_doc1.row_name):
-							tab[i].logo_unit_cost = cost_doc1.logo_unit_cost
-							tab[i].logo_unit_selling = cost_doc1.logo_unit_cost*multiplier
+
+	# Get all Logo Costing Forms that have valid links
+	cost_doc = frappe.get_all("Logo Costing Form",
+		filters={"logo_costing_id": ["!=", ""], "row_name": ["!=", ""]},
+		fields=["name", "logo_costing_id", "row_name", "logo_unit_cost"]
+	)
+
+	# Group changes by parent doc
+	grouped_updates = defaultdict(list)
+	for row in cost_doc:
+		grouped_updates[row["logo_costing_id"]].append(row)
+
+	# Only touch each parent doc once
+	for parent_id, updates in grouped_updates.items():
+		try:
+			doc = frappe.get_doc("TSC Logo Costing", parent_id)
+			changed = False
+			for item in updates:
+				for line in doc.logos:
+					if line.name == item["row_name"]:
+						line.logo_unit_cost = item["logo_unit_cost"]
+						line.logo_unit_selling = item["logo_unit_cost"] * multiplier
+						changed = True
+			if changed:
 				doc.save(ignore_permissions=True)
+		except Exception:
+			frappe.log_error(frappe.get_traceback(), f"Logo Costing Update Failed: {parent_id}")
 	
 
 				
