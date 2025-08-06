a5754d383adce738b016eef60467d97e51c210ca
frogfootdxb <135420034+frogfootdxb@users.noreply.github.com>
2025-08-06 10:02:39 +0000

    updated script with removed patch

--- qcs_invoice_advance/patches/add_is_subcontracted_column.py
+++
@@ -1,8 +0,0 @@
-import frappe
-
-def execute():
-    if not frappe.db.has_column("Stock Entry", "is_subcontracted"):
-        frappe.db.sql("""
-            ALTER TABLE `tabStock Entry`
-            ADD COLUMN `is_subcontracted` INT(1) DEFAULT 0
-        """)
